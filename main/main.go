95b002924c26a6cbee7c4c9e44a96d1e933a7dc2
Animesh Kumar <animuz111@gmail.com>
2020-05-13 05:20:04 +0530

    Improved example
    Signed-off-by: Animesh Kumar <animuz111@gmail.com>

--- main/main.go
+++ main/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	// Open db
-	store, err:= kv.Open("/path/to/store.db")
+	store, err:= kv.Open("./store.db")
 
 	if err!=nil{
 		fmt.Println("Error is: #v", err)
@@ -20,12 +20,20 @@ func main() {
 
 	// store a object
 	store.Put("sess-341356", sessionVal)
-
+	fmt.Println("Value inserted")
+	var val string
 	// Retrieve object
-	store.Get("sess-341356", &sessionVal)
+	store.Get("sess-341356", &val)
+	fmt.Println("Value retrieve Value.")
+	fmt.Println("Value is", val)
 
 	// delete key
 	store.Delete("sess-341356")
+	fmt.Println("Value deleted")
+	var delVal string
+
+	err =store.Get("sess-341356", &delVal)
+	fmt.Println(err)
 
 	// Close db
 	store.Close()
